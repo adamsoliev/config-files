d745a01f3e10c0eb5c356b81f799d5b883b072e4
Adam <asoliyev13@gmail.com>
2023-02-27 07:35:28 -0600

    commented copy local bin script

--- copylbin.sh
+++ copylbin.sh
@@ -6,4 +6,5 @@ dest_dir=/home/adam/dev/config_files/local_bin/
 
 # copy files from source to destination directory
 cp -r $src_dir/* $dest_dir/
+# find all symlinks in the directory and delete them
 find $dest_dir/ -type l -delete
